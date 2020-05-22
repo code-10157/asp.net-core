d070f889265c7d4246911e78f9a554c023bee338
m_kita <m_kita@tos.co.jp>
2020-05-22 16:27:28 +0900

    チュートリアルのやつ

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // スラッシュ3つ　ドキュメント記法
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        //public string Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            //return "This is the Welcome action method...";
+            //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+            //ビューを参照させている
+        }
+
+
+        // GET: /HelloWorld/ohayou/ 
+        //
+        public string ohayou()
+        {
+            return "おはようメソッド";
+            //localhost:44309/helloWorld/welcome?name=Mike&numtimes=3
+            //名前と数値を指定→Hello Mike, NumTimes is: 3
+            //何も指定しない場合→Hello , NumTimes is: 1　
+        }
+    }
+}
---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }　//日付
+
+        //[Column(TypeName = "varchar(10)")]
+        public string Genre { get; set; } //種類
+        
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; } //価格
+    }
+}
