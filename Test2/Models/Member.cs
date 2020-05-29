262c557c93de10f0af8c9f2a0302d68d92f9b564
m_kita <m_kita@tos.co.jp>
2020-05-29 11:32:29 +0900

    DisplayNameを使ってカラム名称変更

--- Test2/Models/Member.cs
+++ Test2/Models/Member.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace Test2.Models
 {
     public class Member
     {
     public int Id { get; set; }
+//表示名称を変えるなら→displayname
+    [DisplayName("名前")]
     public string Name{ get; set; }
     public string Emai{ get; set; }
     public DateTime Birth { get; set; }
