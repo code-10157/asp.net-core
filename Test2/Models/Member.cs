af0583a33dce719cbfa876b14e3c0149b8a890db
m_kita <m_kita@tos.co.jp>
2020-05-29 17:25:00 +0900

    マイグレ失敗

--- Test2/Models/Member.cs
+++ Test2/Models/Member.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,12 +13,21 @@ namespace Test2.Models
     public int Id { get; set; }
 //表示名称を変えるなら→displayname
     [DisplayName("名前")]
+    //↓主キー
+    //[Key]
     public string Name{ get; set; }
     public string Emai{ get; set; }
     public DateTime Birth { get; set; }
     public bool Married { get; set; }
+    //↓最大値
+    [MaxLength(10)]
     public string Memo { get; set; }
-
+ //   [DisplayName("登録日時")]
+ //   public string DateTime { get; set; }
+ //   [DisplayName("タイムスタンプ")]
+    //↓using System.ComponentModel.DataAnnotations;
+//    [Timestamp]
+//    public Byte[] Timestamp { get; set; }
 
     }
 }
