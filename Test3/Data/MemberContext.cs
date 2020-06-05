ca140a0368856e020404cec07b22223afdbe3faf
m_kita <m_kita@tos.co.jp>
2020-06-05 10:13:14 +0900

    モデル追加

---
+++ Test3/Data/MemberContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Test3.Models
+{
+    public class MemberContext : DbContext
+    {
+        public MemberContext (DbContextOptions<MemberContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Test3.Models.Member> Member { get; set; }
+    }
+}
