7612d9947d7cc5fa36830383d91f68bffe2e838d
m_kita <m_kita@tos.co.jp>
2020-06-29 21:55:55 +0900

    tutorial_group

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Required]
+        //[StringLength(50)]
+        [Display(Name = "Last Name")]
+        [StringLength(50, MinimumLength = 2)]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Column("FirstName")]
+        public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }//集計プロパティ
+
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
